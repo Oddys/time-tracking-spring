a089191bd0a75a7ed2862dd4688f7c771aa07d81
Oddys <yuriy.honcharenko@gmail.com>
2019-09-26 13:19:41 +0300

    Add AuthFilter

--- src/main/java/org/oddys/timetrackingspring/config/WebConfig.java
+++ src/main/java/org/oddys/timetrackingspring/config/WebConfig.java
@@ -1,10 +1,12 @@
 package org.oddys.timetrackingspring.config;
 
 import org.modelmapper.ModelMapper;
+import org.oddys.timetrackingspring.filter.AuthFilter;
 import org.oddys.timetrackingspring.util.AttributeSetter;
 import org.oddys.timetrackingspring.util.EntityMapper;
 import org.oddys.timetrackingspring.util.ParameterValidator;
 import org.oddys.timetrackingspring.util.PasswordManager;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -18,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/index").setViewName("index");
         registry.addViewController("/cabinet").setViewName("cabinet");
         registry.addViewController("/activities").setViewName("activities");
-        registry.addViewController("/user-data").setViewName("user-data");
+        registry.addViewController("/cabinet/user-data").setViewName("user-data");
     }
 
     @Bean
@@ -44,5 +46,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public EntityMapper entityMapper() {
         return new EntityMapper(passwordManager());
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthFilter> authFilter() {
+        FilterRegistrationBean<AuthFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new AuthFilter());
+        bean.addUrlPatterns("/cabinet/*");
+        return bean;
     }
 }
---
+++ src/main/java/org/oddys/timetrackingspring/filter/AuthFilter.java
@@ -0,0 +1,49 @@
+package org.oddys.timetrackingspring.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.oddys.timetrackingspring.dto.UserDto;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@Slf4j
+public class AuthFilter implements Filter {
+    private static final Set adminPages = Set.of("/cabinet/user-data");
+    private static final Set userPages = Set.of();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        UserDto user = (UserDto) req.getSession().getAttribute("user");
+        if (user == null) {
+            resp.sendRedirect(req.getContextPath());
+        } else if (adminPages.contains(req.getServletPath())
+                && !"ADMIN".equals(user.getRoleName())) {
+            resp.sendRedirect(req.getContextPath() + "/cabinet");
+        } else if (userPages.contains(req.getServletPath())
+                && !"USER".equals(user.getRoleName())) {
+            resp.sendRedirect(req.getContextPath() + "/cabinet");
+        }
+        chain.doFilter(request, response);
+    }
+}
